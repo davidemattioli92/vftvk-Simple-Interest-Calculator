58b80b4828a5528eb7e5d5ec5bc38f5ad97f46c3
davidemattioli92 <94492589+davidemattioli92@users.noreply.github.com>
2021-11-28 14:47:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    n = document.getElementById("years").value;
-    let r_adj = r/100
-    let result = p*r_adj*n;
+    let p = document.getElementById("principal").value;
+    let r = document.getElementById("rate").value;
+    let n = document.getElementById("years").value;
+    return result = p*r_adj*n/100;
 }
         
