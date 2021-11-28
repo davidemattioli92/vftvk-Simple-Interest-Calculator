be0d4414d9393a5e6240252c00a0cf5fe0f6fdff
davidemattioli92 <94492589+davidemattioli92@users.noreply.github.com>
2021-11-28 15:41:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ function compute()
     let r = document.getElementById("rate").value;
     let n = document.getElementById("years").value;
     let calc =  p * r * n / 100;
-    document.getElementById('calc').innerHTML="If you deposit: " + p + "at an interest rate of: " + r + "You will receive an amount of: " + n + "in the year";
+    document.getElementById('calc').innerHTML="If you deposit: " + p + "at an interest rate of: " + r + "You will receive an amount of: " + calc + "in the year";
 }
         
