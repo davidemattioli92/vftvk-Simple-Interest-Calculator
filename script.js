1de7a90b2ae1102675a37612611600ccc37c719f
davidemattioli92 <94492589+davidemattioli92@users.noreply.github.com>
2021-11-28 15:56:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function compute()
 {
-    let p = document.getElementById("principal").value;
-    let r = document.getElementById("rate").value;
-    let n = document.getElementById("years").value;
-    let calc =  p * r * n / 100;
-    document.getElementById('calc').innerHTML="Result is: " + calc ;
-}
-        
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+    var resultDisplay = document.getElementById("result");
+    resultDisplay.innerHTML = "If you deposit: " + "<span class='highlight'>" + principal + "</span>."  + ", <br> at an interest rate of: "+ "<span class='highlight'>" + rate + "</span>%." + "<br> You will receive an amount of: " + "<span class='highlight'>" + interest + "</span>" + ", <br> in the year " + "<span class='highlight'>" + yearNow + "</span>";
+}     
